e1c2180c954cf30f7e033a3795ed03857290e6e5
togamonro-ru <a.togasawa.sys22@morijyobi.ac.jp>
2024-03-27 10:09:45 +0900

    アカウントの追加

--- teamA/database/migrations/0001_01_01_000000_create_users_table.php
+++ teamA/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('store_number');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('main_flg')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
